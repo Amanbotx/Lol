629cd8ace01d2f5af60e77860c4a4b5cc8dfec7c
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-02-18 06:53:54 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1552,7 +1552,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         else:
             gtxt = "ɢᴏᴏᴅ ɴɪɢʜᴛ 👋"
         await query.message.edit_text(
-            text=script.START_TXT.format(query.from_user.mention, gtxt, message.from_user.id, temp.U_NAME, temp.B_NAME),
+            text=script.START_TXT.format(message.from_user.mention, gtxt, message.from_user.id, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
